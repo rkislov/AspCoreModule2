75707e8cbeee75f76143aa576b37d71039ced65f
roman@kislovs.ru <roman@kislovs.ru>
2017-02-01 09:20:14 +0500

    finish

--- src/AspCoreModule2/Startup.cs
+++ src/AspCoreModule2/Startup.cs
@@ -7,13 +7,22 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace AspCoreModule2
 {
     public class Startup
     {
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+
+            public IConfiguration Configuration { get; set; }
         public void ConfigureServices(IServiceCollection services)
         {
         }
@@ -27,10 +36,10 @@ namespace AspCoreModule2
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            var Welcome = Configuration["Welcome"];
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(Welcome);
             });
         }
     }
